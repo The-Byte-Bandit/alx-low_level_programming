90b7d5796ee13844217f693b4f14c408ca27073c
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-10 00:32:26 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
+	char low;
 
-for(char i = 'a'; i<='z'; i ++){
-	    putchar(i);
-}
-for(char i = 'A'; i<='Z'; i ++){
-	    putchar(i);
-}
-putchar('\n');
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
 	return (0);
 }
