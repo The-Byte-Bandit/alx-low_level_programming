19aad60c1f5ef28c49d0b7cd962a26a2c9e035e8
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-14 00:03:54 +0100

    Create 6-abs.c

---
+++ 6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int);
+int main(void)
+{
+
+    return (0);
+}
+
+int _abs(int n)
+{
+    return abs(n);
+}
