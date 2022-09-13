2f0f5f733ba4dafde0bc07b7305fc3350bc326b2
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-13 23:47:09 +0100

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description:
+ *'The program prints the sign of a number.'
+ * Return: Always 0 (Success)
+ */
+
+int  print_sign( int n);
+int main(void)
+{
+   int us =  6;
+   print_sign(us);
+    return (0);
+}
+
+int  print_sign (int n)
+{
+    if(n > 0)
+    {
+
+        printf("\n +");
+        return (1);
+    }else if (n == 0)
+    {
+        printf("\n %d", 0);
+        return (0);
+    }  
+    else
+    {
+        printf("\n -");
+        return (-1);
+    }
+}
