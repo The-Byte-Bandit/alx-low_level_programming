0cd304a10cb9ce8abdff8d58e2f32ad7b4f0d6a7
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-14 23:54:24 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,47 +7,18 @@
 void jack_bauer(void)
 {
 
-	int i;
-	int b;
-	char g = '0';
+	int i, b;
 
 	for (i = 0; i < 24; i++)
 	{
 		for (b = 0; b < 60; b++)
 		{
-			if (b < 10 && i < 10)
-			{
-				_putchar(g);
-				_putchar(i + '0');
-				_putchar('\t');
-				_putchar(g);
-				_putchar(b);
-				_putchar('\n');
-			}
-			else if (b > 9 && i < 10)
-			{
-				_putchar(g);
-				_putchar(i + '0');
-				_putchar('\t');
-				_putchar(b);
-				_putchar('\n');
-			}
-			else if (i > 9 && b < 10)
-			{
-				_putchar(i + '0');
-				_putchar('\t');
-				_putchar(g);
-				_putchar(b + '0');
-				_putchar('\n');
-			}
-
-			else
-			{
-				_putchar(i + '0');
-				_putchar('\t');
-				_putchar(b + '0');
-				_putchar('\n');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 			
 		}
 	}
