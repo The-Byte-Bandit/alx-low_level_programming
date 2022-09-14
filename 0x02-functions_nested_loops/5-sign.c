86b437c95b523e156764f03e45f5160d1cd3cbc0
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-14 19:44:05 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,12 @@
 #include "main.h"
- /** print_sign - Check Holberton
+ /** 
+ * print_sign - Check Description
  * @n: An input number
  * Description: This function prints the sign of the number
  * Return: 1 if number is positive, o if number is 0, or
  * -1 if number is negative
  */
+
 int print_sign(int n)
 {
 	int value;
