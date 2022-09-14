4b68d4c0b929e3189df6d71925ae0426c0afb515
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-14 15:15:15 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,22 @@
-int main(void)
-{
+#include "main.h"
 
-	    for(int i = 0; i < 10; i++ )
-		        {
-				        for (char ch = 'a'; ch <= 'z'; ch++)
-						        {
-								            putchar(ch);
-									            }
-					        putchar('n');
-						    }
-	        
-	        return (0);
+/**
+ * print_alphabet_x10 - Check description
+ * Description: It prints the alphabet 10x
+ * Return: Nothing.
+ */
+
+
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for(i = 0; i < 10; i++ )
+	{
+	for (char ch = 'a'; ch <= 'z'; ch++);
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	}
 }
