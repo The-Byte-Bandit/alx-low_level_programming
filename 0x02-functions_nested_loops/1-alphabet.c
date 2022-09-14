ae7dff6939f27b31e348f39e6e4e29384b995ec5
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-14 13:15:03 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - Entry point
- *   * Description:
- *    *'The program prints  all english letters'
- *     * Return: Always 0 (Success)
- *      */
+ *  main - Entry point
+ *  Description:
+ * 'The program prints  all english letters'
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	 char put[8]  = "_putchar";
