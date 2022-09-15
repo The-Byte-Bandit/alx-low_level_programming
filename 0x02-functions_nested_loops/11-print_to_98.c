1be24a57983cf0ae5b72c81d3d656c9cd798ec08
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-15 14:58:31 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Check description
+ * @n: input integer
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+ void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n; n <= 98; n++)
+		{
+      _putchar(n);
+
+			if (n == 98)
+				continue;
+			_putchar(',');
+		}
+    _putchar('\n');
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			_putchar(n);
+
+			if (n == 98)
+				continue;
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
