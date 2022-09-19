5ffde7252c5981acc75863de31ec576b7da466f9
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-19 16:03:34 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_rev - prints in reverse.
+ * @s: address of variable s
+ *
+ * Return: null.
+*/
+
+void print_rev(char *s)
+{
+	int i, g;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		g++;
+	}
+	for (g; g >= 0; g--)
+	{
+	_putchar(s[g]);
+	}
+	_putchar('\n');
+}
