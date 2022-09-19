3b3b73871b32d9b357963b4accb830cfc7438f73
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-19 14:54:12 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ *  _strlen -returns length of array.
+ * @c: address of variable c
+ *
+ * Return: nothing
+*/
+
+int _strlen(char *s);
+{
+	int i = 0;
+	int g = 0;
+
+	for(i; s[i] = '\0'; i++)
+	{
+		g++;
+	}
+	return (g);
+}
