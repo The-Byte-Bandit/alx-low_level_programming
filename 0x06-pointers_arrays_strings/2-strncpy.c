3d55805a5f44fc0f57246a7752d3d1f6d476cb18
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-27 14:17:09 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,29 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i, dest_len = 0;
 
-		for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest_len++;
+	}
+	i = 0;
+
+	if	(n < dest_len)
+	{
+		for (i = 0; i < n; i++)
+		{
 			dest[i] = src[i];
-		for ( ; i < n; i++)
-			dest[i] = '\0';
+		}
+	}
+	else
+	{
+		for (i = 0; i < n && src[i] != '\0'; i++)
+		{
+
+			dest[i] = src[i];
+		}
+		dest[i + 1] = '\0';
+	}
 	return (dest);
 }
