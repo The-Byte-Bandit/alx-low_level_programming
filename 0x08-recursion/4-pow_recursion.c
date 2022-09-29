3dc3ada38ed19ce3f03f3a3dfb7c73baac994f98
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-29 18:12:45 +0100

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that returns x to power y.
+ * @x: base number.
+ * @y: incrementor.
+ * Return: Power of x.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		{
+			return (x);
+		}
+		else if (y < 0)
+		{
+			return ( -1);
+		}
+
+	x *= _pow_recursion(x, y - 1);
+	return (x);
+}
