a8a05fae9226804a84df11014477257fc88cc8de
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-09-30 16:35:13 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * squrecheck - a function that checks for square root.
  * _sqrt_recursion - this function runs another function which checks for square root.
@@ -7,24 +6,29 @@
  * @i: an input integer.
  * Return: Natural square root.
  */
+
+/**
+ * _sqrt_recursion - Runs another function which checks for square root.
+ * @n: an input integer.
+ * Return: Natural square root.
+ */
 int squrecheck(int n, int i)
 {
-    if (i < 1)
-    {
-        return (-1);
-    }
-    else if (i * i == n)
-        return (i);
-    n = squrecheck(n, i - 1);
-    return (n);
+	if (i == n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+		return (i);
+	n = squrecheck(n, i + 1);
+	return (n);
 }
-
 int _sqrt_recursion(int n)
 {
-    if (n < 1)
-    {
-        return (-1);
-    }
-    n = squrecheck(n, n);
-    return (n);
+	if (n < 1)
+	{
+		return (-1);
+	}
+	n = squrecheck(n, 1);
+	return (n);
 }
