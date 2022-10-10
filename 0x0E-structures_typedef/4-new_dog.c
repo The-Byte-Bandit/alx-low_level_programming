9635c413ff7dba7a78f5a6e6d4df050d58776f3a
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-10-11 00:28:32 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,47 +1,52 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Return: returns a pointer to newly created dog structure
+ * new_dog - function that creates a new dog.
+ * @name: character pointer
+ * @owner: character pointer
+ * @age: integer
+ * Return: Always 0.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	int a, b, c;
+	dog_t *doggo;
 
-	if (name == NULL || owner == NULL)
+	for (a = 0; name[a] != '\0'; a++)
+		;
+	a++;
+	for (b = 0; owner[b] != '\0'; b++)
+		;
+	b++;
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == 0)
+	{
+		free(doggo);
 		return (NULL);
-
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	}
+	doggo->name = malloc(a * sizeof(doggo->name));
+	if (doggo->name == 0)
+	{
+		free(doggo->name);
+		free(doggo);
 		return (NULL);
-
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
+	}
+	for (c = 0; c < a; c++)
+		doggo->name[c] = name[c];
+	doggo->name[c] = name[c];
+	doggo->age = age;
+	doggo->owner = malloc(b * sizeof(doggo->owner));
+	if (doggo->owner == 0)
+	{
+		free(doggo->owner);
+		free(doggo->name);
+		free(doggo);
 		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	return (new_dog);
+	}
+	for (c = 0; c < b; c++)
+		doggo->owner[c] = owner[c];
+	doggo->owner[c] = owner[c];
+	return (doggo);
 }
