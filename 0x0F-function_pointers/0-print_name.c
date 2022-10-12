fd62fa387d977d122e548f20592f0503a7fdbf4a
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-10-12 13:54:56 +0100

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+*  print_name - A function that prints a name.
+*@name: string to check
+*@f: callback funcction
+*Return: 1 is numm, 0 not num
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
