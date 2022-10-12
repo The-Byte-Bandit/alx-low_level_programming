bf5f007ff3f0753c431aa097ccde361f6e30dbbe
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-10-12 21:52:53 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (cmp == NULL || array == NULL)
-		return (-1);
-
 	if (size <= 0)
 		return (-1);
 
+	
+	if (array != NULL && action != NULL)
 	for (; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
-
+	
 	if (i == size)
 		return (-1);
 
