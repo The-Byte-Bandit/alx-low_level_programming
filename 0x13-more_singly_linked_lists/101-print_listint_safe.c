776558edc321d757a7b30bcc7975a5870803afe4
anotherweirdo1 <100517331+anotherweirdo1@users.noreply.github.com>
2022-10-31 01:17:52 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,34 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
+ * print_listint_safe - prints a list
+ * @head: pointer to the list
+ * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
+	const listint_t *new, *comp;
+	size_t count_new = 0, count_comp = 0;
 
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
+	new = head;
+	comp = head;
+	while (new != NULL)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
+		while (count_new > count_comp)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			if (new == comp)
+			{
+				printf("-> [%p] %d\n", (void *)new, new->n);
+				return (count_new);
+			}
+			count_comp++;
+			comp = comp->next;
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
+		comp = head;
+		count_comp = 0;
+		printf("[%p] %d\n", (void *)new, new->n);
+		count_new++;
+		new = new->next;
 	}
-	head = NULL;
-	return (nodes);
+	return (count_new);
 }
